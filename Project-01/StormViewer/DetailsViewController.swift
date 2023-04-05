5be7ce190006939448607e694e316b3350a87858
kadirhankeles <44638560+kadirhankeles@users.noreply.github.com>
2023-04-05 13:31:27 +0300

    Add files via upload

---
+++ Project-01/StormViewer/DetailsViewController.swift
@@ -0,0 +1,53 @@
+//
+//  DetailsViewController.swift
+//  StormViewer
+//
+//  Created by Kadirhan Keles on 28.03.2023.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+    var chosenImage = ""
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupUI()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+    func setupUI(){
+        view.backgroundColor = .white
+        view.addSubview(imageView)
+        
+        title = chosenImage
+        navigationItem.largeTitleDisplayMode = .never
+        
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: self.view.topAnchor),
+            imageView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+            imageView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+        ])
+    }
+    
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleToFill
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(named: chosenImage)
+        return imageView
+    }()
+
+   
+
+}
