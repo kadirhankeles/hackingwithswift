9464d7589f5858c45c81b4574c8dfc9785d45dc7
kadirhankeles <44638560+kadirhankeles@users.noreply.github.com>
2023-04-19 17:50:50 +0300

    Add files via upload

---
+++ Project-09/Project-07/CustomTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  CustomTableViewCell.swift
+//  Project-07
+//
+//  Created by Kadirhan Keles on 13.04.2023.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Project-09/Project-07/Petitions.swift
@@ -0,0 +1,11 @@
+//
+//  Petitions.swift
+//  Project-07
+//
+//  Created by Kadirhan Keles on 13.04.2023.
+//
+
+import Foundation
+struct Petitions: Codable {
+    var results: [PetitionModel]
+}
